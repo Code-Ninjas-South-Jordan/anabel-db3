e35235dc05a321f449f90a1d8147b18f0e52a68c
CodeNinjas <codeninjassouthjordan@gmail.com>
2021-08-17 17:32:29 -0600

    njkkjk

--- anabel-db3/Assets/Scripts/GameManager.cs
+++ anabel-db3/Assets/Scripts/GameManager.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    //private Spawner spawner;
+    private Spawner spawner;
+    //public GameObject title;
     void Start()
     {
-       //spawner.active = false; 
+       spawner.active = false;
+       //title.SetActive(true); 
     }
     void Awake()
     {
-        //spawner = GameObject.Find("Spawner").GameComponent<Spawner>();
+        spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
     }
     // Update is called once per frame
     void Update()
     {
         if(Input.anyKeyDown)
         {
-            //spawner.active = true;
+            spawner.active = true;
+            //title.SetActive(false);
         }
     }
 }
--- anabel-db3/Assets/Scripts/Spawner.cs
+++ anabel-db3/Assets/Scripts/Spawner.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class spawner : MonoBehaviour
+public class Spawner : MonoBehaviour
 {
     [Header("Bomb Prefab")]
     public GameObject bombPrefab;
