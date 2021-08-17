1ad6ef1d75fbe11827d10fde70b9aaa32501fce0
CodeNinjas <codeninjassouthjordan@gmail.com>
2021-08-17 17:12:28 -0600

    sszxds

--- anabel-db3/Assets/Scripts/GameManager.cs
+++ anabel-db3/Assets/Scripts/GameManager.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    private Spawner spawner;
+    //private Spawner spawner;
     void Start()
     {
-       spawner.active = false; 
+       //spawner.active = false; 
     }
     void Awake()
     {
-        spawner = GameObject.Find("Spawner").GameComponent<Spawner>();
+        //spawner = GameObject.Find("Spawner").GameComponent<Spawner>();
     }
     // Update is called once per frame
     void Update()
     {
         if(Input.anyKeyDown)
         {
-            spawner.active = true;
+            //spawner.active = true;
         }
     }
 }
