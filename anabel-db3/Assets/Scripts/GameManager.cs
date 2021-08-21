a114e2e5a4a8849d280804808c7b49a4fb062613
CodeNinjas <codeninjassouthjordan@gmail.com>
2021-08-21 12:04:58 -0600

    beta 3

--- anabel-db3/Assets/Scripts/GameManager.cs
+++ anabel-db3/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -10,6 +11,11 @@ public class GameManager : MonoBehaviour
     public GameObject playerPrefab;
     public GameObject player;
     private bool gameStarted = false;
+    public GameObject splash;
+    public GameObject scoreSystem;
+    public Text scoreText;
+    public int pointsWorth = 1;
+    private int score;
     void Start()
     {
        spawner.active = false;
@@ -20,6 +26,7 @@ public class GameManager : MonoBehaviour
         spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         player = playerPrefab;
+        scoreText.enabled = false;
     }
     // Update is called once per frame
     void Update()
@@ -39,9 +46,12 @@ public class GameManager : MonoBehaviour
         }
         var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
         foreach(GameObject bombObject in nextBomb)
-        {
-            if(bombObject.transform.position.y < (-screenBounds.y) - 12)
+        {   if(!gameStarted)
             {
+                Destroy(bombObject);
+            } else if(bombObject.transform.position.y < (-screenBounds.y) && gameStarted)
+            {
+                scoreSystem.GetComponent<Score>().AddScore(pointsWorth);
                 Destroy(bombObject);
             }
         }
@@ -52,11 +62,15 @@ public class GameManager : MonoBehaviour
         title.SetActive(false);
         player = Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotation);
         gameStarted = true;
+        splash.SetActive(false);
+        scoreText.enabled = true;
+        scoreSystem.GetComponent<Score>().score = 0;
+        scoreSystem.GetComponent<Score>().Start();
     }
     void OnPlayerKilled()
     {
         spawner.active = false;
         gameStarted = false;
-        //splash.SetActive(true);
+        splash.SetActive(true);
     }
 }
